7597a929a751b5a1169a45b72a111e83b42e8334
Noah Alderton <noahlouisalderton@gmail.com>
2019-08-18 00:17:58 -0400

    The display works now!

--- chip8.cpp
+++ chip8.cpp
@@ -71,6 +71,7 @@ void chip8::cycle()
         case 0x0000: // 0x00E0: Clears the screen
         {
             clearDisplay();
+            drawFlag = true;
             pc += 2;
         }
         break;
@@ -154,21 +155,25 @@ void chip8::cycle()
         case 0x0000: // 8XY0: Sets VX to the value of VY
         {
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x00f0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0001: // 8XY1: Sets VX to VX OR VY.
         {
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x0F00) >> 8] | V[(instruction & 0x00f0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0002: //8XY2: Sets VX to VX AND VY
         {
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x0F00) >> 8] & V[(instruction & 0x00f0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0003: //8XY3: Sets VX to VX XOR VY
         {
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x0F00) >> 8] ^ V[(instruction & 0x00f0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0004: //8XY4: Adds VY to VX
@@ -181,12 +186,14 @@ void chip8::cycle()
             {
                 V[15] = 0;
             }
+
             V[(instruction & 0x0F00) >> 8] += V[(instruction & 0x00f0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0005: //8XY5: VY is subtracted from VX
         {
-            if (V[(instruction & 0x0F00) >> 8] > V[(instruction & 0x00f0) >> 4])
+            if (V[(instruction & 0x0F00) >> 8] < V[(instruction & 0x00F0) >> 4])
             {
                 V[15] = 1;
             }
@@ -194,18 +201,20 @@ void chip8::cycle()
             {
                 V[15] = 0;
             }
-            V[(instruction & 0x0F00) >> 8] -= V[(instruction & 0x00f0) >> 4];
+            V[(instruction & 0x0F00) >> 8] -= V[(instruction & 0x00F0) >> 4];
+            pc += 2;
         }
         break;
         case 0x0006: //8XY6: Stores the least significant bit of VX in VF and then shifts VX to the right by 1
         {
             V[16] = V[(instruction & 0x0F00) >> 8] & 1;
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x0F00) >> 8] >> 1;
+            pc += 2;
         }
         break;
         case 0x0007: //8XY7: Sets VX to VY minus VX. VF is set to 0 when there's a borrow, and 1 when there isn't
         {
-            if (V[(instruction & 0x0F00) >> 4] > V[(instruction & 0x00f0) >> 8])
+            if (V[(instruction & 0x0F00) >> 8] < V[(instruction & 0x00F0) >> 4])
             {
                 V[15] = 1;
             }
@@ -213,13 +222,15 @@ void chip8::cycle()
             {
                 V[15] = 0;
             }
-            V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x00f0) >> 4] - V[(instruction & 0x0F00) >> 8];
+            V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x00F0) >> 4] - V[(instruction & 0x0F00) >> 8];
+            pc += 2;
         }
         break;
         case 0x000E: //8XYE: Stores the most significant bit of VX in VF and then shifts VX to the left by 1
         {
             V[16] = V[(instruction & 0x0F00) >> 8] & 0x80;
             V[(instruction & 0x0F00) >> 8] = V[(instruction & 0x0F00) >> 8] << 1;
+            pc += 2;
         }
         break;
         default:
@@ -228,7 +239,6 @@ void chip8::cycle()
         }
         break;
         }
-        pc += 2;
         break;
     case 0x9000: // 9XY0: Skip next instruction if VX != VY
     {
@@ -323,11 +333,13 @@ void chip8::cycle()
     }
     break;
     case 0xF000:
+    {
         switch (instruction & 0x00FF)
         {
         case 0x0007: // FX07: Sets VX to the value of the delay timer
         {
             V[(instruction & 0x0F00) >> 8] == delay_timer;
+            pc += 2;
         }
         break;
         case 0x000A: // FX0A: A key press is awaited, and then stored in VX
@@ -343,11 +355,13 @@ void chip8::cycle()
         case 0x0015: // FX15: Sets the delay timer to VX
         {
             delay_timer = V[(instruction & 0x0F00) >> 8];
+            pc += 2;
         }
         break;
         case 0x0018: // FX18: Sets the sound timer to VX
         {
             sound_timer = V[(instruction & 0x0F00) >> 8];
+            pc += 2;
         }
         break;
         case 0x001E: //FX1E: Adds VX to I
@@ -361,11 +375,13 @@ void chip8::cycle()
                 V[15] = 0;
             }
             I += V[(instruction & 0x0F00) >> 8];
+            pc += 2;
         }
         break;
         case 0x0029: //FX29: Sets I to the location of the sprite for the character in VX
         {
             I = V[(instruction & 0x0F00) >> 8] * 4;
+            pc += 2;
         }
         break;
         case 0x0033: //FX33: Stores the binary-coded decimal representation of VX
@@ -374,13 +390,15 @@ void chip8::cycle()
             memory[I] = numString.at(0) - 48;
             memory[I + 1] = numString.at(1) - 48;
             memory[I + 2] = numString.at(2) - 48;
+            pc += 2;
         }
-            break;
+        break;
         case 0x0055: //FX55: Register dump at address I from V0 to VX
         {
             for (int i = 0; i < (V[(instruction & 0x0F00) >> 8] + 1); i++)
             {
                 memory[I + i] = V[i];
+                pc += 2;
             }
         }
         break;
@@ -389,6 +407,7 @@ void chip8::cycle()
             for (int i = 0; i < (V[(instruction & 0x0F00) >> 8] + 1); i++)
             {
                 V[i] = memory[I + i];
+                pc += 2;
             }
         }
         break;
@@ -397,9 +416,9 @@ void chip8::cycle()
         {
             printf("Unknown instruction: 0x%X\n", instruction);
         }
-        break;
         }
-        pc += 2;
+        break;
+    }
     default:
     {
         printf("Unknown instruction: 0x%X\n", instruction);
